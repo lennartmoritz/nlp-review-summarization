71e343b5469baae2232a22bc6745e287d8f2ecfe
Timon Engelke <timon.engelke@online.de>
2023-01-31 10:14:38 +0100

    Fix count of topics

--- review_summarization/topic_modelling.py
+++ review_summarization/topic_modelling.py
@@ -1,3 +1,4 @@
+import numpy as np
 from bertopic import BERTopic
 import pandas as pd
 
@@ -9,11 +10,12 @@ def model_topics(data: pd.DataFrame, print_topics: bool = True):
     # Execute the topic modeling
     topic_model = BERTopic(language="english")
     topics, probs = topic_model.fit_transform(docs)
+    topics = np.array(topics)
 
     # Print topic results. The -1 topic refers to all outlier documents and is typically ignored
-    topic_info = topic_model.get_topic_info()
+    topic_count = len(np.unique(topics[topics != -1]))
 
-    print(f"The reviews were clustered into {len(topic_info) - 1} topics.")
+    print(f"The reviews were clustered into {topic_count} topics.")
 
     if print_topics:
         # Print some representative reviews for each topic
